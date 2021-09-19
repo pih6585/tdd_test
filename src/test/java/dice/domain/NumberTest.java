d40da8bd114ac0a83fdd8c672172a8a75b86be64
user <kth4878@naver.com>
2021-09-19 17:22:50 +0900

    test : player 도메인 수정으로 인한 테스트 수정

---
+++ src/test/java/dice/domain/NumberTest.java
@@ -0,0 +1,36 @@
+package dice.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class NumberTest {
+
+	private static final String NUMBER_TYPE_ERROR_MESSAGE = "숫자가 아닙니다.";
+
+	@Test
+	@DisplayName("참가자 인원수 생성")
+	public void createPlayerNumber() {
+		Number number = new Number(2);
+		assertThat(number).isEqualTo(new Number(2));
+	}
+
+	@Test
+	@DisplayName("참가자 인원수 타입에러 발생")
+	public void playerNumberError() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			new Number(checkNumberType("3"));
+			fail("참가자 인원수 입력 에러가 발생해야한다.");
+		});
+	}
+
+	private int checkNumberType(Object inputNumber) {
+		if (!(inputNumber instanceof Integer)) {
+			throw new IllegalArgumentException(NUMBER_TYPE_ERROR_MESSAGE);
+		}
+		return ((Integer)inputNumber).intValue();
+	}
+
+}
