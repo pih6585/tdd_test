f4afec1c3bb0201cc106606986e34a2a6d5e0ed9
user <kth4878@naver.com>
2021-09-27 17:31:50 +0900

    test : 테스트 코드 수정

--- src/test/java/dice/domain/NumberTest.java
+++ src/test/java/dice/domain/NumberTest.java
@@ -9,12 +9,21 @@ import org.junit.jupiter.api.Test;
 public class NumberTest {
 
 	private static final String NUMBER_TYPE_ERROR_MESSAGE = "숫자가 아닙니다.";
+	private static final int MIN_NUMBER = 2;
+	private static final int MAX_NUMBER = 4;
 
 	@Test
 	@DisplayName("참가자 인원수 생성")
 	public void createPlayerNumber() {
 		Number number = new Number(2);
 		assertThat(number).isEqualTo(new Number(2));
+	}
+
+	@Test
+	@DisplayName("참가자 인원수 경계값 확인")
+	public void checkPlayerNumber(){
+		Number number = new Number(2);
+		assertThat(number.getNumber() >= MIN_NUMBER && number.getNumber() <= MAX_NUMBER).isTrue();
 	}
 
 	@Test
